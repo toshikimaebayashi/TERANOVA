0235315400527d55dab61ee540896509c33b9ec1
前林俊希 <toshiki@maehayashishunnozominoMacBook-Pro.local>
2019-11-19 01:25:59 +0900

    jsのクラス名と、HTMLのクラス名をリンクさせる

--- src/js/components/container/Bottom.tsx
+++ src/js/components/container/Bottom.tsx
@@ -4,14 +4,12 @@ class Bottom extends React.Component {
 
  render() {
     return (
-      <div>
-       <div className="MainContainer-bottombtn">
-         <div className="btn-container">
-           <a className="act-button">制作依頼</a>
-         </div>
-         <div className="btn-container">
-           <a className="act-button">お問い合わせ</a>
-         </div>
+      <div className="MainContainer-bottom">
+        <div className="btn-container">
+          <a className="act-button">制作依頼</a>
+        </div>
+        <div className="btn-container">
+          <a className="act-button">お問い合わせ</a>
         </div>
       </div>
     );
