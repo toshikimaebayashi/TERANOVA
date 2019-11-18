101184dd94609c63ed5c178b2589d3598b9d7f8f
前林俊希 <toshiki@maehayashishunnozominoMacBook-Pro.local>
2019-11-18 17:12:32 +0900

    ボタンの修正とcommon.scssの追加

--- src/js/components/container/BottomContent.tsx
+++ src/js/components/container/BottomContent.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 class BottomContent extends React.Component {
-  
+
  render() {
     return (
       <div>
        <div className="MainContainer-bottombtn">
-         <div className="btn-container float-left">
+         <div className="btn-container">
            <a className="act-button">制作依頼</a>
          </div>
-         <div className="btn-container float-right">
+         <div className="btn-container">
            <a className="act-button">お問い合わせ</a>
          </div>
         </div>
       </div>
-      
+
     );
   }
 }
