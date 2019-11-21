78cda6f21b64df9afe7b8bfea129b6f4f8203dcf
前林俊希 <toshiki@maehayashishunnozominoMacBook-Pro.local>
2019-11-21 23:18:20 +0900

    footerの修正

--- src/js/components/container/Bottom.tsx
+++ src/js/components/container/Bottom.tsx
@@ -4,14 +4,14 @@ class Bottom extends React.Component {
 
  render() {
     return (
-      <footer>
+      <div class="footer">
         <div className="btn-container">
           <a className="act-button">制作依頼</a>
         </div>
         <div className="btn-container">
           <a className="act-button">お問い合わせ</a>
         </div>
-      </footer>
+      </div>
     );
   }
 }
