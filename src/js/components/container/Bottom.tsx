8cc5226bc3aa5144962af28f171e5f4ddd9c4229
前林俊希 <toshiki@maehayashishunnozominoMacBook-Pro.local>
2019-11-23 11:54:47 +0900

    footerの修正

--- src/js/components/container/Bottom.tsx
+++ src/js/components/container/Bottom.tsx
@@ -5,11 +5,11 @@ class Bottom extends React.Component {
  render() {
     return (
       <div class="footer">
-        <div className="btn-container">
-          <a className="act-button">制作依頼</a>
+        <div className="footer-container">
+          <a className="footer-btn">制作依頼</a>
         </div>
-        <div className="btn-container">
-          <a className="act-button">お問い合わせ</a>
+        <div className="footer-container">
+          <a className="footer-btn">お問い合わせ</a>
         </div>
       </div>
     );
